015ee13e1048b89afe8340e88c834d07b3bcec47
JamilsonMello <jamilsonmelow@gmail.com>
2020-08-25 21:23:22 -0300

    editFood and change avalability food

--- src/components/Food/index.tsx
+++ src/components/Food/index.tsx
@@ -17,21 +17,25 @@ interface IProps {
   food: IFoodPlate;
   handleDelete: (id: number) => {};
   handleEditFood: (food: IFoodPlate) => void;
+  handleUpdateFood: (food: IFoodPlate) => void;
 }
 
 const Food: React.FC<IProps> = ({
   food,
   handleDelete,
   handleEditFood,
+  handleUpdateFood,
 }: IProps) => {
   const [isAvailable, setIsAvailable] = useState(food.available);
 
   async function toggleAvailable(): Promise<void> {
-    // TODO UPDATE STATUS (available)
+    setIsAvailable(!isAvailable);
+    const foodUpdated = { ...food, available: !food?.available };
+    handleUpdateFood(foodUpdated);
   }
 
   function setEditingFood(): void {
-    // TODO - SET THE ID OF THE CURRENT ITEM TO THE EDITING FOOD AND OPEN MODAL
+    handleEditFood(food);
   }
 
   return (
